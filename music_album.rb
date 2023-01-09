bf6c413a99c74f1b033af0f3a0cde7f7d7969fba
ouasamine <ouas.amine@outlook.com>
2023-01-09 19:15:33 +0100

    Solve rubocop correctable errors

--- music_album.rb
+++ music_album.rb
@@ -2,12 +2,17 @@ require_relative 'item'
 
 class MusicAlbum < Item
   attr_accessor :on_spotify
+
   def initialize(on_spotify)
+    super
     @on_spotify = on_spotify
   end
 
-  private def can_be_archived?
+  private
+
+  def can_be_archived?
     return true if super && @on_spotify
+
     false
   end
 end

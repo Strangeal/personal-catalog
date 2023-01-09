b21ee2293df9656ebaf8345cd83057f8da96c0bc
Ndikumana Isaie <ndikumanaisaie@gmail.com>
2023-01-09 12:21:50 +0100

    Add can_be_archived method logic

--- item.rb
+++ item.rb
@@ -1,5 +1,14 @@
+require 'Date'
 class Item 
   def initialize
 
+  end
+
+  def can_be_archived?
+    (Date.today - Date.parse(@published_date)) >= 365 * 10
+  end 
+
+  def move_to_archived
+    
   end
 end

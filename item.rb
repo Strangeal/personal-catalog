34636f07f100b7f2f73bfc49839de9fc2d23aa3c
ouasamine <ouas.amine@outlook.com>
2023-01-09 13:01:06 +0100

    Create Item class properties

--- item.rb
+++ item.rb
@@ -1,5 +1,12 @@
 class Item 
+  attr_accessor :author, :source, :label, :genre
   def initialize
-
+    @id = Random.rand(1..1000)
+    @author = nil
+    @source = nil
+    @label = nil
+    @genre = nil
+    @publish_date = Date.today
+    @archived = false
   end
 end

dcc917c1aa9dfd5339a92f1b6718b3c24ed9af85
Ndikumana Isaie <ndikumanaisaie@gmail.com>
2023-01-09 13:05:15 +0100

    Fix offenses

--- item.rb
+++ item.rb
@@ -1,5 +1,5 @@
 require 'Date'
-class Item 
+class Item
   def initialize
     @published_date = nil
     @archived = false
@@ -7,7 +7,7 @@ class Item
 
   def can_be_archived?
     (Date.today - Date.parse(@published_date)).to_i >= 365 * 10
-  end 
+  end
 
   def move_to_archive
     @archived = true if can_be_archived?

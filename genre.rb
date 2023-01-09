db0421f135f068e3a5d75483a4b4ea099e087ced
ouasamine <ouas.amine@outlook.com>
2023-01-09 18:43:35 +0100

    Create MusicAlbum & Genre classes with properties

---
+++ genre.rb
@@ -0,0 +1,11 @@
+class Genre
+  def initialize(name)
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+
+  end
+end
---
+++ music_album.rb
@@ -0,0 +1,11 @@
+require 'item'
+
+class MusicAlbum < Item
+  def initialize
+    @on_spotify = nil
+  end
+
+  private def can_be_archived?
+
+  end
+end

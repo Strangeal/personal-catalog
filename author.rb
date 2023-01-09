886e3e5c1163204b191b4ab6077ab49cb5370829
strangeal <bajerijustice@gmail.com>
2023-01-09 17:24:34 -0500

    solve linters

--- author.rb
+++ author.rb
@@ -4,6 +4,7 @@ class Author < Item
   attr_accessor :first_name, :second_name, :items
 
   def initialize(first_name, second_name)
+    super
     @int = Random.rand(1..1000)
     @first_name = first_name
     @second_name = second_name
--- game.rb
+++ game.rb
@@ -4,12 +4,14 @@ class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
   def initialize(multiplayer, last_played_at)
+    super
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
 
   def can_be_archived?
-    return true if super() &&  last_played_at >= 365 * 2 
+    return true if can_be_archived? && last_played_at >= 365 * 2
+
     false
   end
 end

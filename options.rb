a15d52259435bc297c1166f3b914ddbf24d17df9
Ndikumana Isaie <ndikumanaisaie@gmail.com>
2023-01-10 11:00:08 +0100

    Call book_label functions in app.rb

--- options.rb
+++ options.rb
@@ -9,10 +9,10 @@ class Options
     when 3 then puts 'List all movies'
     when 4 then puts app.list_games
     when 5 then puts 'List all genres'
-    when 6 then puts 'List all labels'
+    when 6 then App.new.list_all_labels
     when 7 then puts app.list_authors
     when 8 then puts 'List all sources'
-    when 9 then puts 'Add a book'
+    when 9 then puts App.new.add_book
     when 10 then puts 'Add a music album'
     when 11 then puts 'Add a movie'
     when 12 then puts app.add_game
